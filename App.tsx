0cd1bec864db007d3913eb9adad704cb0831eec4
Sander Ulset <sanderulset1@gmail.com>
2021-11-18 17:58:26 +0100

    Screens and navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { InfectedScreen } from './screens/InfectedScreen';
+import { VaccinatedScreen } from './screens/VaccinatedScreen';
+import { CountriesScreen } from './screens/CountriesScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Infected" component={InfectedScreen} />
+        <Tab.Screen name="Vaccinated" component={VaccinatedScreen} />
+        <Tab.Screen name="Countries" component={CountriesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
---
+++ screens/CountriesScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const CountriesScreen = () => {
+  return (
+    <View>
+      <Text>Countries</Text>
+    </View>
+  );
+};
