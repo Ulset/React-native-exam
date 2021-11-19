ee7efbf3306e18d464dcb7e9a5926541b3ecc17a
Sander Ulset <sanderulset1@gmail.com>
2021-11-19 01:12:32 +0100

    Modal

--- components/SelectCountryModal.tsx
+++ components/SelectCountryModal.tsx
@@ -1,7 +1,7 @@
-import { Button, Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import React from 'react';
 
-export const SelectCountryModal = ({visible, setModalVisible, setCountry}: props) => {
+export const SelectCountryModal = ({ visible, setModalVisible, setCountry }: props) => {
   return (
     <>
       <Modal
@@ -10,36 +10,35 @@ export const SelectCountryModal = ({visible, setModalVisible, setCountry}: props
         visible={visible}
       >
         <TouchableWithoutFeedback onPressIn={() => setModalVisible(false)}>
-          <View style={styles.topView}/>
+          <View style={styles.topView} />
         </TouchableWithoutFeedback>
         <View style={styles.containerView}>
           <Text>Hei</Text>
         </View>
       </Modal>
-  </>
+    </>
 
   );
 };
 
 const styles = StyleSheet.create({
   topView: {
-    flex: 1,
-    backgroundColor: '#EFEFEF'
+    flex: 1
   },
   containerView: {
     backgroundColor: '#FFFFFF',
     borderRadius: 15,
     flex: 2,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 12,
+      height: 12
     },
     shadowOpacity: 0.58,
     shadowRadius: 16.00,
-    elevation: 24,
+    elevation: 24
   }
-})
+});
 
 interface props {
   visible: boolean;
